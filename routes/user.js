fe2a8ecf15c060d523e27d8cdbfff90c5bf4807d
Ali Cangül <97682611+Alicngl@users.noreply.github.com>
2022-09-27 13:05:22 +0300

    dynamic page edit

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,39 @@
 const express = require("express");
 const router = express.Router();
 
+const data = {
+  title: "Popüler Kurslar",
+  list_group: [
+    "Web Geliştirme",
+    "Mobil Uygulamalar",
+    "Veri Analizi",
+    "Programlama",
+  ],
+  blogs: [
+    {
+      id: 1,
+      title: "Komple Uygulamalı Web Geliştirme",
+      explain:
+        " Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass,Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+      img: "1.jpeg",
+    },
+    {
+      id: 2,
+      title: " Python ile Sıfırdan İleri Seviye Python Programlama",
+      explain:
+        "  Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
+      img: "2.jpeg",
+    },
+    {
+      id: 3,
+      title: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
+      explain:
+        "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular,React ve VueJs için sağlam bir temel oluşturun.",
+      img: "3.jpeg",
+    },
+  ],
+};
+
 const path = require("path");
 
 router.use("/blogs/:blogid", function (req, res) {
@@ -8,11 +41,11 @@ router.use("/blogs/:blogid", function (req, res) {
 });
 
 router.use("/blogs", function (req, res) {
-  res.render("users/blogs");
+  res.render("users/blogs", data);
 });
 
 router.use("/", function (req, res) {
-  res.render("users/index");
+  res.render("users/index", data);
 });
 console.log("asdasd");
 
