bf48f38c4fd91cefca0e08596a70c6d827de0991
Ali Cangül <97682611+Alicngl@users.noreply.github.com>
2022-09-30 09:41:16 +0300

    category page added ,one to many added

--- routes/user.js
+++ routes/user.js
@@ -3,40 +3,70 @@ const router = express.Router();
 
 const db = require("../data/db");
 
-router.use("/blogs/:blogid", function (req, res) {
-  res.render("users/blog-details");
+router.use("/blogs/category/:categoryid", async function (req, res) {
+  const id = req.params.categoryid;
+  try {
+    const [blogs] = await db.execute("select * from blog where categoryid=?", [
+      id,
+    ]);
+    const [categories] = await db.execute("select * from category");
+
+    res.render("users/blogs", {
+      title: "Tüm Kurslar",
+      blogs: blogs,
+      categories: categories,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.use("/blogs", function (req, res) {
-  db.execute("select * from blog where onay=1")
-    .then((result) => {
-      db.execute("select * from category")
-        .then((sonuc) => {
-          res.render("users/blogs", {
-            title: "Tüm Kurslar",
-            blogs: result[0],
-            categories: sonuc[0],
-          });
-        })
-        .catch((err) => console.log(err));
-    })
-    .catch((err) => console.log(err));
+router.use("/blogs/:blogid", async function (req, res) {
+  const id = req.params.blogid;
+  try {
+    const [blogs] = await db.execute("select * from blog where blogid=?", [id]);
+    const blog = blogs[0];
+
+    if (blog) {
+      return res.render("users/blog-details", {
+        title: blog.baslik,
+        blogs: blog,
+      });
+    }
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.use("/", function (req, res) {
-  db.execute("select * from blog where onay=1 and anasayfa=1")
-    .then((result) => {
-      db.execute("select * from category")
-        .then((sonuc) => {
-          res.render("users/index", {
-            title: "Popüler Kurslar",
-            blogs: result[0],
-            categories: sonuc[0],
-          });
-        })
-        .catch((err) => console.log(err));
-    })
-    .catch((err) => console.log(err));
+router.use("/blogs", async function (req, res) {
+  try {
+    const [blogs] = await db.execute("select * from blog where onay=1");
+    const [categories] = await db.execute("select * from category");
+    res.render("users/blogs", {
+      title: "Tüm Kurslar",
+      blogs: blogs,
+      categories: categories,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.use("/", async function (req, res) {
+  try {
+    const [blogs] = await db.execute(
+      "select * from blog where onay=1 and anasayfa = 1"
+    );
+    const [categories] = await db.execute("select * from category");
+    res.render("users/index", {
+      title: "Tüm Kurslar",
+      blogs: blogs,
+      categories: categories,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
